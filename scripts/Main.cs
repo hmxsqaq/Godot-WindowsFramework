72baec2a6c71e757ed8580b2d95b0dd4aca9dc5f
hmxs <hmxsqaq@gmail.com>
2025-07-30 00:51:33 +0800

    [init]

---
+++ scripts/Main.cs
@@ -0,0 +1,14 @@
+using Godot;
+
+namespace windows_framework.scripts;
+
+public partial class Main : Control
+{
+	public override void _Ready()
+	{
+	}
+
+	public override void _Process(double delta)
+	{
+	}
+}
