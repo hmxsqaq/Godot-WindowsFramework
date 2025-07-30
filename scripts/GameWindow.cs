de2f2628aa9e6661069c54017abe7e9d315c8495
hmxsqaqwu <hmxsqaqwu@tencent.com>
2025-07-30 18:01:23 +0800

    [dev] window move

--- scripts/GameWindow.cs
+++ scripts/GameWindow.cs
@@ -7,10 +7,20 @@ public partial class GameWindow : Window
     [Export] private Label _idLabel;
     [Export] private Label _posLabel;
     [Export] private Label _sizeLabel;
+    
+    [Export] private Button _topButton;
+    [Export] private Button _bottomButton;
+    [Export] private Button _leftButton;
+    [Export] private Button _rightButton;
 
     public override void _Ready()
     {
         CloseRequested += QueueFree;
+        
+        _topButton.ButtonDown += () => StartResize(DisplayServer.WindowResizeEdge.Top);
+        _bottomButton.ButtonDown += () => StartResize(DisplayServer.WindowResizeEdge.Bottom);
+        _leftButton.ButtonDown += () => StartResize(DisplayServer.WindowResizeEdge.Left);
+        _rightButton.ButtonDown += () => StartResize(DisplayServer.WindowResizeEdge.Right);
     }
 
     public override void _Process(double delta)
